828d70b36393440f29e824d9b514cd903cdb586d
Kago <kmotlhabane769@gmail.com>
2024-02-28 16:02:27 +0200

    Add github icon and link to footer

--- src/ui/layout_component_outlines/footer.js
+++ src/ui/layout_component_outlines/footer.js
@@ -1,9 +1,22 @@
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+
 let createFooter = function() {
     let body = document.querySelector('body');
 
     let footer = document.createElement('footer');
-    footer.textContent = 'Footer';
+    footer.textContent = 'Made by ';
 
+    let selfCredit = document.createElement('a');
+    selfCredit.href = "https://github.com/kaglet/restaurant_page";
+    selfCredit.textContent = "kaglet";
+    let githubIcon = document.createElement('i');
+    githubIcon.classList.add('fa-brands', 'fa-github');
+    selfCredit.append(' ', githubIcon);
+    
+    footer.append(selfCredit);
     body.append(footer);
 };
 
