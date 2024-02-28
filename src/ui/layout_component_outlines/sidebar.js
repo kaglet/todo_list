9cf9d938a6428b858c928bc438c8076265a5b629
Kago <kmotlhabane769@gmail.com>
2024-02-28 16:18:36 +0200

    Add components in sidebar layout

--- src/ui/layout_component_outlines/sidebar.js
+++ src/ui/layout_component_outlines/sidebar.js
@@ -5,6 +5,30 @@ let createSidebar = function() {
     sidebar.classList.add('sidebar');
     sidebar.textContent = 'Sidebar';
 
+    let menuIcon = document.createElement('div');
+    menuIcon.classList.add('icon', 'menu');
+    menuIcon.textContent = "+";
+
+    let quickAddBtn = document.createElement('button');
+    quickAddBtn.textContent = 'Quick Add';
+    let customAddBtn = document.createElement('button');
+    customAddBtn.textContent = 'Custom Add';
+    let nameInput = document.createElement('input');
+    nameInput.setAttribute('placeholder', 'Example Name');
+
+    let buttonWrapper = document.createElement('div');
+    buttonWrapper.classList.add('wrapper');
+
+    buttonWrapper.append(quickAddBtn, customAddBtn);
+
+    let listsWrapper = document.createElement('div');
+    let allListOption = document.createElement('button');
+    allListOption.textContent = 'All';
+
+    listsWrapper.append(allListOption);
+
+    sidebar.append(menuIcon, nameInput, buttonWrapper, listsWrapper);
+
     body.append(sidebar);
 };
 
