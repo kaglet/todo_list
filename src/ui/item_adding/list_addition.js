51981759bf1742b96c1691bc5022acc195627ee1
Kago <kmotlhabane769@gmail.com>
2024-03-03 11:53:29 +0200

    Cleanup leftover files and folders added again from git reverts
    I am not sure what was happening during reverts. Files that should have
    been removed were not being during reverts to and back again.

--- src/ui/item_adding/list_addition.js
+++ src/ui/item_adding/list_addition.js
@@ -3,13 +3,6 @@ import listsManager from '../../application_state_logic/all_lists_manager/lists_
 import createSidebar from '../layout_component_outlines/sidebar.js';
 import listCustomizer from '../item_customizers/list_customizer.js';
 
-// TODO: Merge edit and add new list instance as motivated below
-// An edit controller controls new instance or current instance editing, each same pane should allow editing on itself. Each input will anyway and editing of the same instance on click of enter for example. 
-// Or explicit clicking the edit button to bring the pane up if its controlled by that
-// Is it different edit control in each case. Well no right. The pane itself doesn't manage editing on the application state unless its a listener on itself calling a similar method as the button confirmation input would on the boolean flag.
-// I actually haven't thought how an edit controller will work over an addition controller
-// But now I realize they should be one and the same thing technically. Just with different alternate methods called when entering the input details from the form (to add a new instance or edit a current instance)
-
 // Manages addition functionality of new list instances 
 let listAddController = function() {
     let nameInput = createSidebar.getNameInput();
@@ -25,7 +18,7 @@ let listAddController = function() {
         saveButton.textContent = 'Add';
 
         // The extension includes completing the save functionality being filled in not just attached.
-        saveButton.addEventListener('click', () => listAddController.addCustomList()); // use listEditController.editList(); for edit existing list on save
+        saveButton.addEventListener('click', () => listAddController.addCustomList()); // TODO: use listEditController.editList(); for edit existing list on save
     
         addCustomizer.append(saveButton);
 
@@ -66,9 +59,4 @@ let listAddController = function() {
 
 export default listAddController;
 
-// TODO: Simply show created list details (with given name for quick add if there is a staged list) during addition
-// There is no way to access edits with customizer pane with this controller
-// Call quick add todo with a default name on list creation (or rather not just don't use a name if its not given)
-// When customizer pane is brought up it can show created list details (or the list passed through it put simply, to then display that lists details in the shown pane)
-
-// Whatever is altered in subtask edit view is edited at that selected index (good performance from selection tracker being used as always a linear search time)
+// TODO: Whatever is altered in subtask edit view is edited at that selected index (good performance from selection tracker being used as always a linear search time)
