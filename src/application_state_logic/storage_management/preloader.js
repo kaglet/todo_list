af42d9e66c1a06a696ec2b482e77208b68ffd0b1
Kago <kmotlhabane769@gmail.com>
2024-03-25 12:30:33 +0200

    Place instructions for updating UI with initial lists from storage

--- src/application_state_logic/storage_management/preloader.js
+++ src/application_state_logic/storage_management/preloader.js
@@ -1,6 +1,28 @@
-let preloader = function()  {
+import listsManager from "../all_lists_manager/lists_manager";
+import storageManager from "./storage_manager.js";
 
-    return { };
+let preloader = function () {
+    const populateInitialLists = () => {
+        if (!localStorage.getItem("lists")) {
+            // Initialize lists as empty
+            storageManager.saveChanges();
+        } else {
+            storageManager.getAllLists().forEach(list => {
+                listsManager.addList(list);
+            });
+        }
+    };
+
+    const updateDisplayWithLists = () => {
+        // remove default list display in order to place updated list or non-updated list to be sure (no other way to check if default list was edited)
+        // show all lists method if it is there or just show one at a time or remove initial one in display that does not match one in lists manager
+        // Need to use same methods that ensure coherency and a display all lists method would be nice anyway from a list of lists provided as a parameter to make things easy and quick
+        // Update display with the lists actually logically present to put it simply, the entire display of all lists like todos
+    };
+
+    populateInitialLists();
+
+    return {};
 }();
 
 export default preloader;
